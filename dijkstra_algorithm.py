fc71b757cbb5d371ef10e01d47c524f2e5fd2c2b
Zvi-Erblich <144027827+Zvi-Erblich@users.noreply.github.com>
2024-07-30 11:41:37 +0300

    Update dijkstra_algorithm.py

--- dijkstra_algorithm.py
+++ dijkstra_algorithm.py
@@ -3,32 +3,26 @@ import math
 import cv2
 import matplotlib.pyplot as plt
 
-# file = 'maze.jpeg'
-# file = 'maze5.jpeg'
 file = 'maze2.png'
 
-
-img = cv2.imread(file)  # read an image from a file using
-cv2.circle(img, (22, 315), 3, (255, 0, 0), -1)  # add a circle at (5, 220)
-cv2.circle(img, (330, 326), 3, (0, 0, 255), -1)  # add a circle at (5,5)
-plt.figure(figsize=(7, 7))
-plt.imshow(img)  # show the image
+# קריאת התמונה פעם אחת
 img = cv2.imread(file)
-plt.show()
+cv2.circle(img, (22, 315), 3, (255, 0, 0), -1)  # הוספת עיגול בנקודת התחלה
+cv2.circle(img, (330, 326), 3, (0, 0, 255), -1)  # הוספת עיגול בנקודת סיום
+img_copy = img.copy()  # שמירה של עותק של התמונה המקורית
 
 print(img.shape)
 
-
+# הכנת התמונה למבנה גרפי
 img = img[:, :, 0] // 200
 
-
+# פונקציות עזר
 def graph_to_list(image):
     graph = image.tolist()
     for i in range(0, image.shape[0]):
         for j in range(0, image.shape[1]):
             graph[i][j] = [graph[i][j], (i, j), float("inf"), None]
     return graph
-
 
 def neighbors(vertex, image=img):
     neighbors = []
@@ -41,20 +35,15 @@ def neighbors(vertex, image=img):
                 neighbors.append((x, y))
     return neighbors
 
-
 def init_dijkstra(heap, graph, source):
     x, y = source
     graph[x][y][2] = 0
     heapq.heappush(heap, [graph[x][y][2], graph[x][y]])
     return heap
 
-
 def dijkstra(heap, graph, end):
     visited = set((float("inf"), None))
-    # progres = tqdm(225 * 225)
     while end not in visited:
-        # progres.update()
-        # print(len(heap))
         min = heapq.heappop(heap)
         visited.add(min[1][1])
         x_min, y_min = min[1][1]
@@ -76,9 +65,7 @@ def dijkstra(heap, graph, end):
                             graph[x][y][2] = min[1][2] + math.sqrt(2)
                             graph[x][y][3] = min[1][1]
                             heapq.heappush(heap, (graph[x][y][2], graph[x][y]))
-    print(graph)
     return graph
-
 
 def get_path(graph, source, end):
     path = []
@@ -86,9 +73,7 @@ def get_path(graph, source, end):
     while current != source:
         path.append(current)
         current = graph[current[0]][current[1]][3]
-    print(path)
     return path
-
 
 def draw_path(img, path, thickness=1):
     x0, y0 = path[0]
@@ -97,20 +82,15 @@ def draw_path(img, path, thickness=1):
         cv2.line(img, (y0, x0), (y1, x1), (255, 0, 0), thickness)
         x0, y0 = vertex
 
-
+# הרצת הקוד
 empty_heap = []
 graph = graph_to_list(img)
-# source = (5, 25)
-# source = (330, 326)
 source = (155, 18)
 heap = init_dijkstra(empty_heap, graph, source)
-# end = (220, 5)
-# end = (330, 17)
 end = (146, 190)
 graph_1 = dijkstra(heap, graph, end)
 path = get_path(graph_1, source, end)
-img = cv2.imread(file)
-draw_path(img, path)
+draw_path(img_copy, path)
 plt.figure(figsize=(7, 7))
-plt.imshow(img)  # show the image on the screen
+plt.imshow(img_copy)  # הצגת התמונה עם המסלול על המסך
 plt.show()
